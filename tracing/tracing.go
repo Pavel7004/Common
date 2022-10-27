9c6de7a5f6d73bf5f5a7c5e08949b77705a3b3c2
Kovalev Pavel <kovalev5690@gmail.com>
2022-10-27 20:38:41 +0300

    Add `InitDefaultJaeger()` func

--- tracing/tracing.go
+++ tracing/tracing.go
@@ -2,11 +2,39 @@ package tracing
 
 import (
 	"context"
+	"io"
+
+	opentracing "github.com/opentracing/opentracing-go"
+	jaeger "github.com/uber/jaeger-client-go"
+	jaegercfg "github.com/uber/jaeger-client-go/config"
+	"github.com/uber/jaeger-lib/metrics"
 
 	misc "github.com/Pavel7004/Common/misc"
-	opentracing "github.com/opentracing/opentracing-go"
 )
 
 func StartSpanFromContext(ctx context.Context) (opentracing.Span, context.Context) {
 	return opentracing.StartSpanFromContext(ctx, misc.GetParentFuncName())
+}
+
+func InitDefaultJaeger(serviceName string) io.Closer {
+	cfg := jaegercfg.Configuration{
+		ServiceName: serviceName,
+		Sampler: &jaegercfg.SamplerConfig{
+			Type:  jaeger.SamplerTypeConst,
+			Param: 1,
+		},
+		Reporter: &jaegercfg.ReporterConfig{
+			LogSpans: true,
+		},
+	}
+	jMetricsFactory := metrics.NullFactory
+	tracer, closer, err := cfg.NewTracer(
+		jaegercfg.Logger(nil),
+		jaegercfg.Metrics(jMetricsFactory),
+	)
+	if err != nil {
+		panic(err)
+	}
+	opentracing.SetGlobalTracer(tracer)
+	return closer
 }
